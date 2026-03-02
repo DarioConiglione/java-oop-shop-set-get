e88d46e2c3bd02ec007b28803c5489ecd299704d
DarioConiglione <dariocng13@gmail.com>
2026-03-02 10:21:48 +0100

    play with set on smartphone

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -9,5 +9,14 @@ public class Main {
 
         System.out.println(farina.getMarca());
         System.out.println(burger.getPrezzo());
+
+        Smartphone cellulare1 = new Smartphone("Galaxy S26", "Samsung", 999, 22, "ASD1234FDS", 16);
+        System.out.println(
+                "Questo cellulare è " + cellulare1.name + " e costa esattamente " + cellulare1.prezzo + "euro");
+        cellulare1.setName("IPhone-18");
+        cellulare1.setMarca("Apple");
+        cellulare1.setPrezzo(1300);
+        System.out.println(
+                "Questo cellulare è " + cellulare1.name + " e costa esattamente " + cellulare1.prezzo + "euro");
     }
 }
